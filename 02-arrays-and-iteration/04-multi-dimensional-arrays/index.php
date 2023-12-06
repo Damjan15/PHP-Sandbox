bd19c89b2af46dde7ccc90b796b43be23d8d11e7
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-06 11:24:33 +0100

    02-arrays-and-iteration: lesson 04-multi-dimensional-arrays added

--- 02-arrays-and-iteration/04-multi-dimensional-arrays/index.php
+++ 02-arrays-and-iteration/04-multi-dimensional-arrays/index.php
@@ -1,5 +1,40 @@
 <?php
 $output = null;
+
+// Simple multi-dimensional array
+$fruits = [
+  ['Apple', 'Red'],
+  ['Orange', 'Orange'],
+  ['Banana', 'Yellow']
+];
+
+$output = "The name of the fruit is {$fruits[0][0]}, and the color is {$fruits[0][1]}";
+
+// Adding a fruit
+$fruits[] = ['Grape', 'Purple'];
+
+echo "<pre>";
+print_r($fruits);
+echo '</pre>';
+
+// Multi-Dimensional Associative Arrays
+$users = [
+  ['name' => 'John', 'email' => 'john@gmail.com', 'password' => 'secret'],
+  ['name' => 'Mary', 'email' => 'mary@gmail.com', 'password' => 'secret'],
+  ['name' => 'Jane', 'email' => 'jane@gmail.com', 'password' => 'secret']
+];
+
+$output = "John's email is {$users[0]['email']}";
+
+// Add a User
+$users[] = ['name' => 'Alex', 'email' => 'alex@gmail.com', 'password' => 'secret'];
+
+// Remove a User
+array_pop($users); // removes the last user
+
+// Count Users
+$output = "The number of users on our platform are " . count($users);
+
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +57,13 @@ $output = null;
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
       <!-- Output -->
       <p class="text-xl"><?= $output ?></p>
+
+      <h2 class="text-2xl font-semibold">Users:</h2>
+      <p>
+      <pre>
+          <?php print_r($users); ?>
+        </pre>
+      </p>
     </div>
   </div>
 </body>
