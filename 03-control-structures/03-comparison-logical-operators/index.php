b9e0f0c8ef129a2024d1a24748e385b53dddbe14
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-06 18:10:27 +0100

    03-control-structures: lesson 03-comparison-logical-operators added

--- 03-control-structures/03-comparison-logical-operators/index.php
+++ 03-control-structures/03-comparison-logical-operators/index.php
@@ -1,4 +1,6 @@
 <?php
+$x = 10;
+$y = '10';
 
 /*
 | Comparison Operators
@@ -14,7 +16,16 @@
 | <=       | Less than or equal to    |
 | >=       | Greater than or equal to |
 */
-
+var_dump($x == $y); // true
+var_dump($x === $y); // false
+var_dump($x != $y); // false
+var_dump($x <> $y); // false
+var_dump($x !== $y); // false
+var_dump($x < $y); // false
+var_dump($x > $y); // false
+var_dump($x <= $y); // true
+var_dump($x >= $y); // true
+echo "<br />";
 
 /*
 | Logical Operators
@@ -27,3 +38,11 @@
 | xor      | True if one is true    |
 | !        | True if it is not true |
 */
+$a = 10;
+$b = 20;
+
+var_dump($a == 10 and $b == 20); // true
+var_dump($a == 10 or $b == 20); // true
+var_dump($a == 10 xor $b == 20); // false
+var_dump($a == 10 || $b == 20); // true
+var_dump($a == 10 && $b == 20); // true
