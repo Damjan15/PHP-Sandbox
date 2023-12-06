2151721af9460ccea3098d7efe98ab01447c8e7d
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-06 16:44:04 +0100

    03-control-structures: lesson 01-if-statements added

--- 03-control-structures/01-if-statements/index.php
+++ 03-control-structures/01-if-statements/index.php
@@ -1,10 +1,34 @@
 <?php
+$age = 19;
 
 // If statement
+if ($age >= 21) {
+    echo "You are old enough to drink in the US.";
+}
 
 // If-Else
+if ($age >= 21) {
+    echo "You are old enough to drink in the US.";
+} else {
+    echo "You are not old enough to drink in the US.";
+}
 
 // Nested if statement
+if ($age >= 21) {
+    echo "You are old enough to drink in the US.";
+} else {
+    if ($age >= 18) {
+        echo "You are old enough to vote in the US.";
+    } else {
+        echo "You are not old enough to drink or vote in the US.";
+    }
+}
 
 // If-Else-If
-
+if ($age >= 21) {
+    echo "You are old enough to drink in the US.";
+} elseif ($age >= 18) {
+    echo "You are old enough to vote in the US.";
+} else {
+    echo "You are not old enough to drink or vote in the US.";
+}
