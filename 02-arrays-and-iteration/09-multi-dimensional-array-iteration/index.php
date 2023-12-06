b3bc38510e578c813ac923ea2a5c939cc75f11f4
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-06 12:34:31 +0100

    02-arrays-and-iteration: lesson 09-multi-dimensional-array-iteration added

--- 02-arrays-and-iteration/09-multi-dimensional-array-iteration/index.php
+++ 02-arrays-and-iteration/09-multi-dimensional-array-iteration/index.php
@@ -62,25 +62,27 @@ $listings = [
   </header>
   <div class="container mx-auto p-4 mt-4">
     <!-- Output -->
-    <div class="md my-4">
-      <div class="bg-white rounded-lg shadow-md">
-        <div class="p-4">
-          <h2 class="text-xl font-semibold">Software Engineer</h2>
-          <p class="text-gray-700 text-lg mt-2">We are seeking a skilled software engineer to develop high-quality software solutions.</p>
-          <ul class="mt-4">
-            <li class="mb-2">
-              <strong>Salary:</strong> $80,000
-            </li>
-            <li class="mb-2">
-              <strong>Location:</strong> San Francisco
-            </li>
-            <li class="mb-2">
-              <strong>Tags:</strong> Software Development, Java, Python
-            </li>
-          </ul>
+    <?php foreach ($listings as $job) : ?>
+      <div class="md my-4">
+        <div class="bg-white rounded-lg shadow-md">
+          <div class="p-4">
+            <h2 class="text-xl font-semibold"><?php echo $job['title']; ?></h2>
+            <p class="text-gray-700 text-lg mt-2"><?php echo $job['description']; ?></p>
+            <ul class="mt-4">
+              <li class="mb-2">
+                <strong>Salary:</strong> <?php echo $job['salary']; ?>
+              </li>
+              <li class="mb-2">
+                <strong>Location:</strong> <?php echo $job['location']; ?>
+              </li>
+              <li class="mb-2">
+                <strong>Tags:</strong> <?php echo implode(', ', $job['tags']) ?>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    <?php endforeach; ?>
 
     <div class="md my-4">
       <div class="bg-white rounded-lg shadow-md">
