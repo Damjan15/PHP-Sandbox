01d7c46c4917a84f1411be71d2d52d319f135984
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-06 11:06:43 +0100

    02-arrays-and-iteration: lesson 02-array-functions added

--- 02-arrays-and-iteration/02-array-functions/index.php
+++ 02-arrays-and-iteration/02-array-functions/index.php
@@ -1,5 +1,45 @@
 <?php
 $output = null;
+$ids = [10, 22, 15, 45, 67];
+$users = ['user1', 'user2', 'user3'];
+
+// count
+$output = count($users);
+
+// sort
+sort($ids);
+
+// rsort
+rsort($users);
+
+// array_push
+array_push($users, 'user4');
+array_push($ids, '75');
+
+// array_pop
+array_pop($ids);
+
+// array_shift
+array_shift($users);
+
+// array_unshift
+array_unshift($users, 'user5');
+
+// array_slice
+$ids2 = array_slice($ids, 2, 3);
+
+// array_splice
+array_splice($ids, 1, 1, 'New ID');
+
+// array_reverse
+array_reverse($users);
+
+// array_search
+$output = array_search(22, $ids);
+
+// in_array
+$output = in_array(67, $ids);
+
 ?>
 
 <!DOCTYPE html>
@@ -23,6 +63,27 @@ $output = null;
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
             </p>
+
+            <h2 class="text-xl font-semibold my-4">IDs Array: </h2>
+            <p>
+            <pre>
+                    <?php print_r($ids); ?>
+                </pre>
+            </p>
+
+            <h2 class="text-xl font-semibold my-4">Users Array</h2>
+            <pre>
+                <p>
+                    <?php print_r($users); ?>
+                </p>
+            </pre>
+
+            <h2 class="text-xl font-semibold my-4">New IDs Array: </h2>
+            <pre>
+                <p>
+                    <?php print_r($ids2); ?>
+                </p>
+            </pre>
         </div>
     </div>
 </body>
