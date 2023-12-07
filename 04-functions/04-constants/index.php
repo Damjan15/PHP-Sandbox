31b69b8864a13022677d05dcd540e8c3ac973671
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-07 19:26:53 +0100

    04-functions: lesson 04-constants added

---
+++ 04-functions/04-constants/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Defining a constant
+ */
+
+//  define()
+define("APP_NAME", "My App");
+define("APP_VERSION", "1.0.0");
+
+
+echo APP_NAME;
+
+// const keyword
+const DB_NAME = "mydb";
+const DB_HOST = "localhost";
+
+echo DB_NAME, DB_HOST;
