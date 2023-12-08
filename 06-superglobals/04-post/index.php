55d6f77018443bd4f0bbda1ce1aba035077f7761
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-08 14:08:36 +0100

    06-superglobals: lesson 04-post added

--- 06-superglobals/04-post/index.php
+++ 06-superglobals/04-post/index.php
@@ -1,3 +1,15 @@
+<?php
+$title = "";
+$description = "";
+$submitted = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submit"])) {
+  $title = isset($_POST['title']) ? htmlspecialchars($_POST['title']) : "";
+  $description = isset($_POST['description']) ? htmlspecialchars($_POST['description']) : "";
+  $submitted = true;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,14 +24,16 @@
   <div class="flex justify-center items-center h-screen">
     <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
       <h1 class="text-2xl font-semibold mb-6">Create Job Listing</h1>
-      <form method="post">
+      <form method="post" action="">
         <div class="mb-4">
           <label for="title" class="block text-gray-700 font-medium">Title</label>
-          <input type="text" id="title" name="title" placeholder="Enter job title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none">
+          <input type="text" id="title" name="title" placeholder="Enter job title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none" value="<?php echo $title; ?>">
         </div>
         <div class="mb-6">
           <label for="description" class="block text-gray-700 font-medium">Description</label>
-          <textarea id="description" name="description" placeholder="Enter job description" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none"></textarea>
+          <textarea id="description" name="description" placeholder="Enter job description" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none">
+            <?php echo $description ?>
+          </textarea>
         </div>
         <div class="flex items-center justify-between">
           <button type="submit" name="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none">
@@ -30,6 +44,19 @@
       </form>
 
       <!-- Display submitted data -->
+      <?php if ($submitted) : ?>
+        <div class="mt-6 p-4 border rounded bg-gray-200">
+          <h2 class="text-lg font-semibold">Submitted Job Listing: </h2>
+          <p>
+            <strong>Title: </strong>
+            <?php echo $title; ?>
+          </p>
+          <p>
+            <strong>Description: </strong>
+            <?php echo $description; ?>
+          </p>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
 </body>
