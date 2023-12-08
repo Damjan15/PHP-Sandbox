8326644940461200375e0e5417457da3ecd9b9b2
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-08 13:55:30 +0100

    06-superglobals: lesson 03-get added

--- 06-superglobals/03-get/index.php
+++ 06-superglobals/03-get/index.php
@@ -1,3 +1,12 @@
 <?php
 // Add a query parameter to the URL in the browser's address bar:
 // http://localhost:8000/?name=John
+
+// Accessing Query String Parameters
+var_dump($_GET);
+echo $_GET['name'];
+
+// Check if Parameter Exists
+if (isset($_GET['name'])) {
+    echo $_GET['name'];
+}
