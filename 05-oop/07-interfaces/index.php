5b14fbee37aa4327266afc65b7c966a834fb3aa4
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-08 13:09:25 +0100

    05-oop: 07-interfaces lesson added & section completed

--- 05-oop/07-interfaces/index.php
+++ 05-oop/07-interfaces/index.php
@@ -1,3 +1,65 @@
+<?php
+
+// Defining inteface
+interface ContentInteface
+{
+  public function display();
+  public function edit();
+}
+
+// Implementing the interace in Article Class
+class Article implements ContentInteface
+{
+  private $title;
+  private $content;
+
+  public function __construct($title, $content)
+  {
+    $this->title = $title;
+    $this->content = $content;
+  }
+
+  public function display()
+  {
+    echo "<h2>{$this->title}</h2>";
+    echo "<p>{$this->content}</p>";
+  }
+
+  public function edit()
+  {
+    echo "Editing the article '{$this->title}'...";
+  }
+}
+
+// Implementing the interface in Video Class
+class Video implements ContentInteface
+{
+  private $title;
+  private $url;
+
+  public function __construct($title, $url)
+  {
+    $this->title = $title;
+    $this->url = $url;
+  }
+
+  public function display()
+  {
+    echo "<h2>{$this->title}</h2>";
+    echo "<iframe src='{$this->url}'></iframe>";
+  }
+
+  public function edit()
+  {
+    echo "Editing the video '{$this->title}'...";
+  }
+}
+
+// Instantiating Objects
+$article = new Article("Introduction to PHP", "PHP is a versatile scripting language...");
+$video = new Video("PHP For Beginners", "https://www.youtube.com/watch?v=BUCiSSyIGGU");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +78,11 @@
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
+      <?php echo $article->display(); ?>
+    </div>
+
+    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+      <?php echo $video->display(); ?>
     </div>
   </div>
 </body>
