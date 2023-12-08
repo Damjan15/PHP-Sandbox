b5a24f76ca8eabce4eaa3b5774190a1357fc982e
Damjan15 <stamenovdamjan5@gmail.com>
2023-12-08 12:45:42 +0100

    05-oop: lesson 03-inheritence added

--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -17,3 +17,31 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+class Admin extends User
+{
+  public $level;
+
+  public function __construct($name, $email, $level)
+  {
+    $this->level = $level;
+    parent::__construct($name, $email); // Calls the parent constructor
+  }
+
+  public function showStatus()
+  {
+    echo $this->status;
+  }
+
+  // Overrindg Method from Parent Class
+  public function login()
+  {
+    echo 'Admin ' . $this->name . ' logged in.';
+  }
+}
+
+$admin1 = new Admin("Tom Smith", "tom@gmail.com", 5);
+echo $admin1->name . "<br>";
+echo $admin1->email . "<br>";
+$admin1->login();
+// $admin1->status;
